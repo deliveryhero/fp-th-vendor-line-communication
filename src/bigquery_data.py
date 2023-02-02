1c044a9bf9155c09e0e8502df98f5606a38c0c5a
LALITNEW <100189691+LALITNEW@users.noreply.github.com>
2023-02-02 14:16:55 +0700

    Update bigquery_data.py

--- src/bigquery_data.py
+++ src/bigquery_data.py
@@ -12,12 +12,23 @@ credentials = service_account.Credentials.from_service_account_file(
 client = bigquery.Client(credentials=credentials, project=credentials.project_id,)
 job_config = bigquery.LoadJobConfig(write_disposition="WRITE_APPEND")
 
+def batch(iterable, n=1):
+    l = len(iterable)
+    for ndx in range(0, l, n):
+        yield iterable[ndx:min(ndx + n, l)]
+
 def query_BQ_table(query):
     job_config_query = bigquery.QueryJobConfig()
     return client.query(query, job_config=job_config_query).to_dataframe()
 
 def record_line_communication_logs(table_id, rows_to_insert):
-    status = client.insert_rows_json(table_id, rows_to_insert)
+    status = []
+    for i in batch(rows_to_insert, 1000):
+        errors = client.insert_rows_json(table_id, i)
+        if errors == []:
+            status.append("New rows have been added.")
+        else:
+            status.append("Encountered errors while inserting rows: {}".format(errors))
     return status
 
 
