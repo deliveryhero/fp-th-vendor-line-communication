bf011722f726140bd18261f93f1c79b400cd32a2
di-th-auto-mattaroj <70569201+MatharaRojanamontien@users.noreply.github.com>
2023-02-20 07:01:36 +0000

    DITH-1465 Update main.py

--- line_no_deal_no_ncr/run.bat
+++ line_no_deal_no_ncr/run.bat
@@ -2,4 +2,4 @@ chcp 65001
 
 set PYTHONIOENCODING=utf-8
 
-py "C:\Users\foodpanda_di\Documents\GitHub\fp-th-vendor-line-communication\line_D7_no_valid_order\main.py"
+py "C:\Users\foodpanda_di\Documents\GitHub\fp-th-vendor-line-communication\line_no_deal_no_ncr\main.py"
