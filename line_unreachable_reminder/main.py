954c9bae6de893e4dc79380835e2ef17ace5b34b
Worawan Thanasanchiraphong <worawan.thanasanchiraphong@foodpanda.co.th>
2023-10-04 11:44:45 +0700

    DITH-1982 Create pipeline for unreachable reminder via Line

---
+++ line_unreachable_reminder/main.py
@@ -0,0 +1,79 @@
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+sys.path.insert(0, parentdir + '/src')
+from bigquery_data import query_BQ_table, record_line_communication_logs
+from send_line_message import send_request_line_api_checkin_reminder
+from get_secrete_token import get_secret_data
+import datetime, pytz
+import requests
+from template import json_object
+
+# Basic configuration tables
+query_table = "foodpanda-th-bigquery.pandata_th.country_TH_vendor_experience_line_unreachable_reminder"
+logs_table_id = "foodpanda-th-bigquery.pandata_th_external.line_communication_logs_live"
+
+# Basic configuration parameters
+slack_webhook = os.getenv('slack_webhook')
+Live = False
+url = "https://api.line.me/v2/bot/message/push"
+
+token = get_secret_data()
+headers = {'Authorization': "Bearer {" + token + "}", 'Content-Type': 'application/json'}
+tz = pytz.timezone('Asia/Bangkok')
+now = datetime.datetime.now(tz).strftime("%Y-%m-%d %H:%M:%S")
+
+if Live == False:
+    query = f"""
+      SELECT
+        vendor_code,
+        "Uca11d4d4585c435204950dba18dafcd8" AS line_user_id
+      FROM {query_table}
+      WHERE line_user_id IS NOT NULL
+        AND DATE(record_created_at_local) = CURRENT_DATE("Asia/Bangkok")
+      LIMIT 1
+    """
+
+if Live == True:
+    query = f"""
+      SELECT
+        vendor_code,
+        line_user_id
+      FROM {query_table}
+      WHERE line_user_id IS NOT NULL
+        AND DATE(record_created_at_local) = CURRENT_DATE("Asia/Bangkok")
+    """
+
+try:
+  dataframe = query_BQ_table(query)
+  # print("Created dataframe successfully")
+except BaseException as e:
+  requests.post(slack_webhook,
+  json = {'text' : '*line_unreachable_reminder*: Failed to get data: ' + str(e)})
+  # print("Cannot create dataframe")
+  # print(e)
+
+try:
+  reponse_code_list, json_list = send_request_line_api_checkin_reminder(url = url,
+                                                          headers = headers,
+                                                          json_object = json_object,
+                                                          dataframe = dataframe)
+except BaseException as e:
+  requests.post(slack_webhook,
+  json = {'text' : '*line_unreachable_reminder*: Failed send API request: ' + str(e)})
+
+df = dataframe.filter(items=['vendor_code', 'line_user_id'])
+df["return_response"] = reponse_code_list
+df["msg_sent_date_time"] = now
+df["template_id_if_any"] = "line_unreachable_reminder"
+df["msg_url"] = url
+df["msg_content"] = "line_unreachable_reminder"
+df_records = df.to_dict('records')
+
+try:
+  status = record_line_communication_logs(logs_table_id, df_records)
+except BaseException as e:
+  requests.post(slack_webhook,
+  json = {'text' : '*line_unreachable_reminder*: Failed to record logs: ' + str(e)})
+  # print(e)
---
+++ line_unreachable_reminder/template.py
@@ -0,0 +1,28 @@
+json_object = """
+
+{
+  "to": "{line_user_id}",
+  "messages": [
+    {
+      "type": "flex",
+      "altText": "foodpanda ไม่สามารถติดต่อคุณได้",
+      "contents": {
+        "type": "bubble",
+        "size": "giga",
+        "hero": {
+          "type": "image",
+          "url": "https://drive.google.com/uc?export=view&id=1UP1ypH9Z2eLEZB5sKKOgmOmRarBSnGF8",
+          "size": "full",
+          "aspectRatio": "104:119",
+          "aspectMode": "cover",
+          "position": "relative"
+        }
+      }
+    }
+  ],
+  "customAggregationUnits": [
+    "unreachable"
+  ]
+}
+
+"""
