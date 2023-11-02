eaaf443dd03d482bda4536fd912d8e64b189b4cd
Worawan Thanasanchiraphong <worawan.thanasanchiraphong@foodpanda.co.th>
2023-11-02 13:08:01 +0700

    DITH-2024 Create pipeline for line_first_failed_too_busy

---
+++ line_first_failed_too_busy/run.bat
@@ -0,0 +1,5 @@
+chcp 65001
+
+set PYTHONIOENCODING=utf-8
+
+py "C:\Users\foodpanda_di\Documents\GitHub\fp-th-vendor-line-communication\line_first_failed_too_busy\main.py"
---
+++ line_first_failed_too_busy/template.py
@@ -0,0 +1,31 @@
+json_object = """
+
+{
+  "to": "{line_user_id}",
+  "messages": [
+    {
+      "type": "flex",
+      "altText": "เคยปฏิเสธออเดอร์เพราะร้านยุ่งใช่ไหม?",
+      "contents": {
+        "type": "bubble",
+        "size": "giga",
+        "hero": {
+          "type": "image",
+          "url": "https://drive.google.com/uc?export=view&id=1w_4Cz00ZHOuZqor7lr6fCr3Q3IGcxwVX",
+          "size": "full",
+          "aspectRatio": "104:187",
+          "aspectMode": "cover",
+          "action": {
+            "type": "uri",
+            "uri": "https://bit.ly/49347Dd"
+          }
+        }
+      }
+    }
+  ],
+  "customAggregationUnits": [
+    "SMS"
+  ]
+}
+
+"""
