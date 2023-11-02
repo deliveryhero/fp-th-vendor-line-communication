ea43f35e56a2d53c0bafd4569c76ba350ad5341a
Worawan Thanasanchiraphong <worawan.thanasanchiraphong@foodpanda.co.th>
2023-11-02 13:52:25 +0700

    DITH-2024 Create a pipeline for line_first_failed_traffic_manager_vendor_closed

---
+++ line_first_failed_traffic_manager_vendor_closed/run.bat
@@ -0,0 +1,5 @@
+chcp 65001
+
+set PYTHONIOENCODING=utf-8
+
+py "C:\Users\foodpanda_di\Documents\GitHub\fp-th-vendor-line-communication\line_first_failed_traffic_manager_vendor_closed\main.py"
---
+++ line_first_failed_traffic_manager_vendor_closed/template.py
@@ -0,0 +1,47 @@
+json_object = """
+
+{
+  "to": "{line_user_id}",
+  "messages": [
+    {
+      "type": "flex",
+      "altText": "เปิดทำการอยู่ แต่ไรเดอร์หาร้านไม่เจอ?!",
+      "contents": {
+        "type": "bubble",
+        "size": "giga",
+        "hero": {
+          "type": "image",
+          "url": "https://drive.google.com/uc?export=view&id=13SxyHvOYqPYC2Tdk6tWWuLGkAWS8_KT7",
+          "size": "full",
+          "aspectRatio": "4:4",
+          "aspectMode": "cover",
+          "action": {
+            "type": "uri",
+            "uri": "https://bit.ly/3ScwlFG"
+          }
+        },
+        "body": {
+          "type": "box",
+          "layout": "vertical",
+          "contents": [
+            {
+              "type": "button",
+              "action": {
+                "type": "uri",
+                "label": "ดูวิธีการเปลี่ยนที่อยู่ร้าน คลิก",
+                "uri": "https://bit.ly/3S7S41r"
+              },
+              "color": "#FF2B85",
+              "style": "primary"
+            }
+          ]
+        }
+      }
+    }
+  ],
+  "customAggregationUnits": [
+    "SMS"
+  ]
+}
+
+"""
