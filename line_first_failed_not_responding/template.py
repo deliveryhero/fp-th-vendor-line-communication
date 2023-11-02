3e3fe23dbfbc54be4c404ddfc4230bebf616729a
Worawan Thanasanchiraphong <worawan.thanasanchiraphong@foodpanda.co.th>
2023-11-02 14:09:41 +0700

    DITH-2024 Create a pipeline for line_first_failed_not_responding

---
+++ line_first_failed_not_responding/template.py
@@ -0,0 +1,31 @@
+json_object = """
+
+{
+  "to": "{line_user_id}",
+  "messages": [
+    {
+      "type": "flex",
+      "altText": "แย่แล้ว! มีออเดอร์ถูกปฏิเสธ!",
+      "contents": {
+        "type": "bubble",
+        "size": "giga",
+        "hero": {
+          "type": "image",
+          "url": "https://drive.google.com/uc?export=view&id=1E6xdsVcdazH00ufswFjTfoAHYy1Mt4Td",
+          "size": "full",
+          "aspectRatio": "4:4",
+          "aspectMode": "cover",
+          "action": {
+            "type": "uri",
+            "uri": "https://bit.ly/3Q90D9E"
+          }
+        }
+      }
+    }
+  ],
+  "customAggregationUnits": [
+    "SMS"
+  ]
+}
+
+"""
