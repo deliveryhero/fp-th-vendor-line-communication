a161c9bac8dfa88c3fb74ad21305e16bd4924aec
Worawan Thanasanchiraphong <worawan.thanasanchiraphong@foodpanda.co.th>
2023-11-02 14:01:26 +0700

    DITH-2024 Create a pipeline for line_first_failed_product_unavailable

---
+++ line_first_failed_product_unavailable/template.py
@@ -0,0 +1,31 @@
+json_object = """
+
+{
+  "to": "{line_user_id}",
+  "messages": [
+    {
+      "type": "flex",
+      "altText": "เข้าปิดเมนูที่หมดเพื่อลดการปฏิเสธออเดอร์เลย!",
+      "contents": {
+        "type": "bubble",
+        "size": "giga",
+        "hero": {
+          "type": "image",
+          "url": "https://drive.google.com/uc?export=view&id=1HFs5RrIseJhHg63EdErlbYTdI1tOcyza",
+          "size": "full",
+          "aspectRatio": "104:185",
+          "aspectMode": "cover",
+          "action": {
+            "type": "uri",
+            "uri": "https://bit.ly/3Q90vXI"
+          }
+        }
+      }
+    }
+  ],
+  "customAggregationUnits": [
+    "SMS"
+  ]
+}
+
+"""
