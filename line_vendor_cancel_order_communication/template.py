04131fb5ddb76cc539be3e505e52dbfc2bb0a7d1
LALITNEW <100189691+LALITNEW@users.noreply.github.com>
2023-12-06 12:51:30 +0700

    commit

---
+++ line_vendor_cancel_order_communication/template.py
@@ -0,0 +1,35 @@
+json_object = """
+
+{
+    "to": "{line_user_id}",
+    "messages": [
+        {
+            "type": "flex",
+            "altText": "ถ้าออเดอร์ถูกยกเลิกจะได้รับเงินไหม",
+            "contents":
+{
+  "type": "bubble",
+  "size": "giga",
+  "hero": {
+    "type": "image",
+    "size": "full",
+    "aspectRatio": "52:36",
+    "aspectMode": "cover",
+    "action": {
+      "type": "uri",
+      "uri": "https://bit.ly/3MxuR56"
+    },
+    "url": "https://drive.google.com/uc?export=view&id=1m1bhhOvPs-J52L3BiMmtMPoXJrHGyF-N"
+  }
+}
+    }
+  ],
+    "customAggregationUnits": [
+        "CancleOrder"
+    ]
+}'
+}
+
+
+
+"""
--- line_vendor_rider_late_communication/run.bat
+++ line_vendor_rider_late_communication/run.bat
@@ -2,4 +2,4 @@ chcp 65001
 
 set PYTHONIOENCODING=utf-8
 
-py "C:\Users\foodpanda_di\Documents\GitHub\fp-th-vendor-line-communication\line_checkin_reminder\main.py"
+py "C:\Users\foodpanda_di\Documents\GitHub\fp-th-vendor-line-communication\line_vendor_rider_late_communication\main.py"
