c038076bc7a1965cb024a3838744e405337604c5
Worawan Thanasanchiraphong <worawan.thanasanchiraphong@foodpanda.co.th>
2023-12-15 15:28:22 +0700

    DITH-2116 Create Incubation Invoice pipeline

---
+++ line_incubation_d15_invoice/template.py
@@ -0,0 +1,32 @@
+json_object = """
+
+{
+  "to": "{line_user_id}",
+  "messages": [
+    {
+      "type": "flex",
+      "altText": "เคลียร์ชัดเรื่องการเงิน และใบแจ้งหนี้",
+      "contents": {
+        "type": "bubble",
+        "size": "giga",
+        "direction": "ltr",
+        "hero": {
+          "type": "image",
+          "url": "https://drive.google.com/uc?export=view&id=1u4UmH9OQj7hjnIPdlsphtzNoUog839XF",
+          "align": "center",
+          "gravity": "center",
+          "size": "full",
+          "aspectRatio": "100:100",
+          "aspectMode": "cover",
+          "action": {
+            "type": "uri",
+            "label": "คลิก",
+            "uri": "http://bit.ly/3lNgxvg"
+          }
+        }
+      }
+    }
+  ]
+}
+
+"""
