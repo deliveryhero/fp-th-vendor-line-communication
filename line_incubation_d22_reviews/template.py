24533ca2a12b0a1dd37799353f76f2ef349c1439
Worawan Thanasanchiraphong <worawan.thanasanchiraphong@foodpanda.co.th>
2023-12-15 15:40:08 +0700

    DITH-2116 Create Incubation Reviews pipeline

---
+++ line_incubation_d22_reviews/template.py
@@ -0,0 +1,49 @@
+json_object = """
+
+{
+  "to": "{line_user_id}",
+  "messages": [
+    {
+      "type": "flex",
+      "altText": "สร้างความน่าเชื่อถือให้ร้านบนโลกออนไลน์ด้วย 'รีวิวลูกค้าและเรทติ้ง",
+      "contents": {
+        "type": "bubble",
+        "size": "giga",
+        "direction": "ltr",
+        "hero": {
+          "type": "image",
+          "url": "https://drive.google.com/uc?export=view&id=1nGVUmRqaImrNGFbtocVCRm4s3X7akHSE",
+          "align": "center",
+          "gravity": "center",
+          "size": "full",
+          "aspectRatio": "100:100",
+          "aspectMode": "cover",
+          "action": {
+            "type": "uri",
+            "label": "คลิก",
+            "uri": "https://www.tiktok.com/@foodpanda_university_th/video/7164340897155435803?is_from_webapp=1&sender_device=pc&web_id=7121913860252124674"
+          }
+        },
+        "footer": {
+          "type": "box",
+          "layout": "horizontal",
+          "contents": [
+            {
+              "type": "button",
+              "action": {
+                "type": "uri",
+                "label": "ดูวิธีใช้เครื่องมือ",
+                "uri": "http://bit.ly/3nt9fgN"
+              },
+              "color": "#FF2B85FF",
+              "height": "sm",
+              "style": "primary"
+            }
+          ]
+        }
+      }
+    }
+  ]
+}
+
+"""
