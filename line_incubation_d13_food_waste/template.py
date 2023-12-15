6825e2fc1b1bd97cffea034948db75faf26e779a
Worawan Thanasanchiraphong <worawan.thanasanchiraphong@foodpanda.co.th>
2023-12-15 15:36:09 +0700

    DITH-2116 Create Incubation Food Waste pipeline

---
+++ line_incubation_d13_food_waste/template.py
@@ -0,0 +1,27 @@
+json_object = """
+
+{
+  "to": "{line_user_id}",
+  "messages": [
+    {
+      "type": "flex",
+      "altText": "ข้อแนะนำพาร์ทเนอร์ เพื่อรักษามาตรฐานร้านค้าให้ลูกค้าเกิดความพึงพอใจสูงสุด",
+      "contents": {
+        "type": "bubble",
+        "size": "giga",
+        "direction": "ltr",
+        "hero": {
+          "type": "image",
+          "url": "https://drive.google.com/uc?export=view&id=1uACRnI0Jwx_F1HQ4OhBnSvKaor_z1nGO",
+          "align": "center",
+          "gravity": "center",
+          "size": "full",
+          "aspectRatio": "520:563",
+          "aspectMode": "cover"
+        }
+      }
+    }
+  ]
+}
+
+"""
