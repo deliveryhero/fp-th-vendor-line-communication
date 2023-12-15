5c066cac76c9cc911f2dc42102ae7baae3095db2
Worawan Thanasanchiraphong <worawan.thanasanchiraphong@foodpanda.co.th>
2023-12-15 15:28:36 +0700

    DITH-2116 Create Incubation IHS pipeline

---
+++ line_incubation_d13_ihs/run.bat
@@ -0,0 +1,5 @@
+chcp 65001
+
+set PYTHONIOENCODING=utf-8
+
+py "C:\Users\foodpanda_di\Documents\GitHub\fp-th-vendor-line-communication\line_incubation_d13_ihs\main.py"
---
+++ line_incubation_d13_ihs/template.py
@@ -0,0 +1,27 @@
+json_object = """
+
+{
+  "to": "{line_user_id}",
+  "messages": [
+    {
+      "type": "flex",
+      "altText": "ทำความรู้จักคะแนนร้านค้ายอดเยี่ยมกัน!",
+      "contents": {
+        "type": "bubble",
+        "size": "giga",
+        "direction": "ltr",
+        "hero": {
+          "type": "image",
+          "url": "https://drive.google.com/uc?export=view&id=1snU8Ok-ursRY6yPgLDlE01kAjgB749fY",
+          "align": "center",
+          "gravity": "center",
+          "size": "full",
+          "aspectRatio": "4:4",
+          "aspectMode": "cover"
+        }
+      }
+    }
+  ]
+}
+
+"""
