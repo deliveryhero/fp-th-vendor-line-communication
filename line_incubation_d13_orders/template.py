88921876cd548f74bb0fe385216889cb8eb841d4
Worawan Thanasanchiraphong <worawan.thanasanchiraphong@foodpanda.co.th>
2023-12-15 15:25:23 +0700

    DITH-2116 Create Incubation Orders pipeline

---
+++ line_incubation_d13_orders/template.py
@@ -0,0 +1,27 @@
+json_object = """
+
+{
+  "to": "{line_user_id}",
+  "messages": [
+    {
+      "type": "flex",
+      "altText": "มือใหม่ควรรู้! วิธีตรวจสอบสถานะของออเดอร์",
+      "contents": {
+        "type": "bubble",
+        "size": "giga",
+        "direction": "ltr",
+        "hero": {
+          "type": "image",
+          "url": "https://drive.google.com/uc?export=view&id=1esT0N7Jug7oqL0bNRI8ADhEs7rKztxyS",
+          "align": "center",
+          "gravity": "center",
+          "size": "full",
+          "aspectRatio": "100:163",
+          "aspectMode": "cover"
+        }
+      }
+    }
+  ]
+}
+
+"""
