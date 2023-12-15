db4e9697993ae29575ae548e2f0b27a7efaa4690
Worawan Thanasanchiraphong <worawan.thanasanchiraphong@foodpanda.co.th>
2023-12-15 15:19:37 +0700

    DITH-2116 Create Incubation Menu Management pipeline

---
+++ line_incubation_d8_menu_mgmt/template.py
@@ -0,0 +1,32 @@
+json_object = """
+
+{
+  "to": "{line_user_id}",
+  "messages": [
+    {
+      "type": "flex",
+      "altText": "5 ขั้นตอน ปั้นร้านให้ปัง สำหรับร้านใหม่เพิ่งเริ่มขาย",
+      "contents": {
+        "type": "bubble",
+        "size": "giga",
+        "direction": "ltr",
+        "hero": {
+          "type": "image",
+          "url": "https://drive.google.com/uc?export=view&id=1SqMuQc0D0JbZdwMqa25Gths6AmTmm8xF",
+          "align": "center",
+          "gravity": "center",
+          "size": "full",
+          "aspectRatio": "100:100",
+          "aspectMode": "cover",
+          "action": {
+            "type": "uri",
+            "label": "คลิก",
+            "uri": "https://bit.ly/3Zn1gij"
+          }
+        }
+      }
+    }
+  ]
+}
+
+"""
