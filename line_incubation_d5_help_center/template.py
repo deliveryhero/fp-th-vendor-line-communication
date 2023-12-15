9d8b999530074329da49c395754908e151bcce73
Worawan Thanasanchiraphong <worawan.thanasanchiraphong@foodpanda.co.th>
2023-12-15 14:59:00 +0700

    DITH-2116 Create Incubation Help Center pipeline

---
+++ line_incubation_d5_help_center/template.py
@@ -0,0 +1,58 @@
+json_object = """
+
+{
+  "to": "{line_user_id}",
+  "messages": [
+    {
+      "type": "flex",
+      "altText": "พบปัญหาในการขาย ติดต่อ foodpanda อย่างไร? ไปดูกัน!",
+      "contents": {
+        "type": "bubble",
+        "size": "giga",
+        "direction": "ltr",
+        "hero": {
+          "type": "image",
+          "url": "https://drive.google.com/uc?export=view&id=1v0uuHbMn31-97wwzyNqurOgLw-KgwmJX",
+          "align": "center",
+          "gravity": "center",
+          "size": "full",
+          "aspectRatio": "100:118",
+          "aspectMode": "cover",
+          "action": {
+            "type": "uri",
+            "label": "คลิก",
+            "uri": "https://bit.ly/47OzTTD"
+          }
+        },
+        "body": {
+          "type": "box",
+          "layout": "vertical",
+          "contents": [
+            {
+              "type": "text",
+              "text": "แก้ไขปัญหาการเงิน หรือใบแจ้งหนี้ คลิก ",
+              "action": {
+                "type": "uri",
+                "label": "action",
+                "uri": "https://youtube.com/playlist?list=PLWLrh3ZaezZwdvA4NCuY9ecdzpDK_Y9zx&si=Ffg6-s6bFMfZyrZa"
+              },
+              "style": "italic",
+              "align": "center"
+            }
+          ],
+          "backgroundColor": "#FFFFFF",
+          "cornerRadius": "lg",
+          "alignItems": "center",
+          "paddingAll": "sm"
+        },
+        "styles": {
+          "body": {
+            "backgroundColor": "#F7C6CC"
+          }
+        }
+      }
+    }
+  ]
+}
+
+"""
