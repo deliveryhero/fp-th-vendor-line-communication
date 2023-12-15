5b758bbd91c6d0fb15a6084b5e78141be1c1e273
Worawan Thanasanchiraphong <worawan.thanasanchiraphong@foodpanda.co.th>
2023-12-15 15:46:48 +0700

    DITH-2116 Create Incubation NPS Survey pipeline

---
+++ line_incubation_d30_nps_survey/main.py
@@ -0,0 +1,83 @@
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+sys.path.insert(0, parentdir + '/src')
+from bigquery_data import query_BQ_table, record_line_communication_logs
+from send_line_message import send_request_line_api_generic_reminder
+from get_secrete_token import get_secret_data
+import datetime, pytz
+import requests
+from template import json_object
+
+# Basic configuration tables
+query_table = "fulfillment-dwh-production.pandata_report.country_TH_vendor_ops_incubation_line_verification"
+logs_table_id = "foodpanda-th-bigquery.pandata_th_external.line_communication_logs_live"
+pipeline_name = "line_incubation_d30_nps_survey"
+vendor_age = 30
+
+# Basic configuration parameters
+slack_webhook = os.getenv('slack_webhook')
+Live = False
+url = "https://api.line.me/v2/bot/message/push"
+
+token = get_secret_data()
+headers = {'Authorization': "Bearer {" + token + "}", 'Content-Type': 'application/json'}
+tz = pytz.timezone('Asia/Bangkok')
+now = datetime.datetime.now(tz).strftime("%Y-%m-%d %H:%M:%S")
+
+if Live == False:
+    query = f"""
+      SELECT
+        vendor_code,
+        "Uca11d4d4585c435204950dba18dafcd8" AS line_user_id,
+        vendor_age
+      FROM `{query_table}`
+      WHERE line_user_id IS NOT NULL
+        AND vendor_age = {vendor_age}
+      LIMIT 1
+    """
+
+if Live == True:
+    query = f"""
+      SELECT
+        vendor_code,
+        line_user_id,
+        vendor_age
+      FROM `{query_table}`
+      WHERE line_user_id IS NOT NULL
+        AND vendor_age = {vendor_age}
+    """
+
+try:
+  dataframe = query_BQ_table(query)
+  # print("Created dataframe successfully")
+except BaseException as e:
+  requests.post(slack_webhook,
+  json = {'text' : '*{pipeline_name}*: Failed to get data: ' + str(e)})
+  # print("Cannot create dataframe")
+  # print(e)
+
+try:
+  reponse_code_list, json_list = send_request_line_api_generic_reminder(url = url,
+                                                          headers = headers,
+                                                          json_object = json_object,
+                                                          dataframe = dataframe)
+except BaseException as e:
+  requests.post(slack_webhook,
+  json = {'text' : '*{pipeline_name}*: Failed send API request: ' + str(e)})
+
+df = dataframe.filter(items=['vendor_code', 'line_user_id'])
+df["return_response"] = reponse_code_list
+df["msg_sent_date_time"] = now
+df["template_id_if_any"] = pipeline_name
+df["msg_url"] = url
+df["msg_content"] = pipeline_name
+df_records = df.to_dict('records')
+
+try:
+  status = record_line_communication_logs(logs_table_id, df_records)
+except BaseException as e:
+  requests.post(slack_webhook,
+  json = {'text' : '*{pipeline_name}*: Failed to record logs: ' + str(e)})
+  # print(e)
---
+++ line_incubation_d30_nps_survey/template.py
@@ -0,0 +1,32 @@
+json_object = """
+
+{
+  "to": "{line_user_id}",
+  "messages": [
+    {
+      "type": "flex",
+      "altText": "เป็นยังไงบ้างกับการเริ่มต้นขายบน foodpanda?",
+      "contents": {
+        "type": "bubble",
+        "size": "giga",
+        "direction": "ltr",
+        "hero": {
+          "type": "image",
+          "url": "https://drive.google.com/uc?export=view&id=1130Hu1yhlYd64EtYvBqY2z_8x5bEni1n",
+          "align": "center",
+          "gravity": "center",
+          "size": "full",
+          "aspectRatio": "100:100",
+          "aspectMode": "cover",
+          "action": {
+            "type": "uri",
+            "label": "คลิก",
+            "uri": "http://bit.ly/40EloxB"
+          }
+        }
+      }
+    }
+  ]
+}
+
+"""
