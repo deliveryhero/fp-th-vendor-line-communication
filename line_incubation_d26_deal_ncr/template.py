c44a003675aa605f210c9a251bbfef30bdedae46
Worawan Thanasanchiraphong <worawan.thanasanchiraphong@foodpanda.co.th>
2023-12-15 15:46:32 +0700

    DITH-2116 Create Incubation Deal & NCR pipeline

---
+++ line_incubation_d26_deal_ncr/template.py
@@ -0,0 +1,63 @@
+json_object = """
+
+
+{
+  "to": "{line_user_id}",
+  "messages": [
+    {
+      "type": "flex",
+      "altText": "มาทำความรู้จักกับเครื่องมือโปรโมทร้านบน foodpanda กันเถอะ!",
+      "contents": {
+        "type": "bubble",
+        "size": "giga",
+        "direction": "ltr",
+        "hero": {
+          "type": "image",
+          "url": "https://drive.google.com/uc?export=view&id=14Z3oO6AQpMEFilvWYmTFATQZjSScC2qm",
+          "align": "center",
+          "gravity": "center",
+          "size": "full",
+          "aspectRatio": "100:100",
+          "aspectMode": "cover",
+          "action": {
+            "type": "uri",
+            "label": "คลิก",
+            "uri": "http://bit.ly/3lOOonw"
+          }
+        },
+        "footer": {
+          "type": "box",
+          "layout": "horizontal",
+          "contents": [
+            {
+              "type": "button",
+              "action": {
+                "type": "uri",
+                "label": "ลองเลย ",
+                "uri": "http://bit.ly/3FSFfRt"
+              },
+              "color": "#FF2B85FF",
+              "height": "sm",
+              "style": "primary",
+              "offsetEnd": "xs"
+            },
+            {
+              "type": "button",
+              "action": {
+                "type": "uri",
+                "label": "เรียนรู้การเงิน ",
+                "uri": "https://youtube.com/playlist?list=PLWLrh3ZaezZwdvA4NCuY9ecdzpDK_Y9zx&si=TH8OAYbIK33RQdaE"
+              },
+              "color": "#FF2B85FF",
+              "height": "sm",
+              "style": "primary",
+              "offsetStart": "xs"
+            }
+          ]
+        }
+      }
+    }
+  ]
+}
+
+"""
