3e6fa0b2d9f85b683ff376fa0ed78c5effbf985a
Worawan Thanasanchiraphong <worawan.thanasanchiraphong@foodpanda.co.th>
2023-12-15 15:31:50 +0700

    DITH-2116 Create Incubation Fail Rate pipeline

---
+++ line_incubation_d21_fail_rate/template.py
@@ -0,0 +1,27 @@
+json_object = """
+
+{
+  "to": "{line_user_id}",
+  "messages": [
+    {
+      "type": "flex",
+      "altText": "ทำไมร้านค้าควรหลีกเลี่ยงการปฏิเสธออเดอร์?",
+      "contents": {
+        "type": "bubble",
+        "size": "giga",
+        "direction": "ltr",
+        "hero": {
+          "type": "image",
+          "url": "https://drive.google.com/uc?export=view&id=1pqW4TOYF_uzib3xsZJYEG5iP3u64LtAd",
+          "align": "center",
+          "gravity": "center",
+          "size": "full",
+          "aspectRatio": "100:100",
+          "aspectMode": "cover"
+        }
+      }
+    }
+  ]
+}
+
+"""
