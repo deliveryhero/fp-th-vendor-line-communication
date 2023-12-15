630e43101c68338aa0ee58570dd05842f7cdf17d
Worawan Thanasanchiraphong <worawan.thanasanchiraphong@foodpanda.co.th>
2023-12-15 14:49:57 +0700

    DITH-2116 Create Incubation Live Orders pipeline

---
+++ line_incubation_d2_live_orders/template.py
@@ -0,0 +1,54 @@
+json_object = """
+
+{
+  "to": "{line_user_id}",
+  "messages": [
+    {
+      "type": "flex",
+      "altText": "จัดการออเดอร์บนแท็บเล็ตโกดรอยได้ง่ายๆ",
+      "contents": {
+        "type": "bubble",
+        "size": "giga",
+        "direction": "ltr",
+        "hero": {
+          "type": "image",
+          "url": "https://drive.google.com/uc?export=view&id=1Yw_IjEZxMJPkU1JyVr7-LNW_ZXo9BvML",
+          "align": "center",
+          "gravity": "center",
+          "size": "full",
+          "aspectRatio": "20:21",
+          "aspectMode": "cover"
+        },
+        "body": {
+          "type": "box",
+          "layout": "vertical",
+          "contents": [
+            {
+              "type": "text",
+              "text": "ดูวิดีโอการจัดการออเดอร์ทุกกรณี คลิก!",
+              "size": "xs",
+              "align": "center",
+              "style": "italic",
+              "action": {
+                "type": "uri",
+                "label": "action",
+                "uri": "https://youtube.com/playlist?list=PLWLrh3ZaezZwslWrTmJ2uXpIVX3wh_F7o&si=qkd4OBzlFQOIzn_m"
+              }
+            }
+          ],
+          "backgroundColor": "#F7C6CC",
+          "cornerRadius": "lg",
+          "paddingAll": "sm",
+          "action": {
+            "type": "uri",
+            "label": "action",
+            "uri": "https://youtube.com/playlist?list=PLWLrh3ZaezZwslWrTmJ2uXpIVX3wh_F7o&si=xKOtOf0PnvEAb9nR"
+          },
+          "alignItems": "center"
+        }
+      }
+    }
+  ]
+}
+
+"""
