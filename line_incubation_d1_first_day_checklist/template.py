8b0ac93f89b01d2fc4e7002be3faa4028e4e6a44
Worawan Thanasanchiraphong <worawan.thanasanchiraphong@foodpanda.co.th>
2023-12-15 11:00:52 +0700

    DITH-2116 Create Incubation First Day Checklist pipeline

---
+++ line_incubation_d1_first_day_checklist/template.py
@@ -0,0 +1,54 @@
+json_object = """
+
+{
+  "to": "{line_user_id}",
+  "messages": [
+    {
+      "type": "flex",
+      "altText": "สำคัญ! เช็กลิส 4 ข้อ เตรียมตัวให้พร้อมก่อนเปิดร้านบน foodpanda",
+      "contents": {
+        "type": "bubble",
+        "size": "giga",
+        "direction": "ltr",
+        "hero": {
+          "type": "image",
+          "url": "https://drive.google.com/uc?export=view&id=1jJChl_Ugg4XvOp60FpFw7Xa5O6nk1hRj",
+          "align": "center",
+          "gravity": "center",
+          "size": "full",
+          "aspectRatio": "20:21",
+          "aspectMode": "cover"
+        },
+        "body": {
+          "type": "box",
+          "layout": "vertical",
+          "contents": [
+            {
+              "type": "text",
+              "contents": [],
+              "action": {
+                "type": "uri",
+                "label": "action",
+                "uri": "https://youtube.com/playlist?list=PLWLrh3ZaezZwvTBYF3oydRjYFQqALze4J&si=vDS7SmjpMUD1vVTT"
+              },
+              "text": "ดูวิดีโอสำหรับมือใหม่เพื่อพร้อมเปิดร้านขาย  คลิก!",
+              "size": "sm",
+              "align": "center",
+              "style": "italic"
+            }
+          ],
+          "cornerRadius": "lg",
+          "backgroundColor": "#FFFFFF",
+          "paddingAll": "sm"
+        },
+        "styles": {
+          "body": {
+            "backgroundColor": "#F7C6CC"
+          }
+        }
+      }
+    }
+  ]
+}
+
+"""
