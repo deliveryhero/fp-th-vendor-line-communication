833e67e98ef76ba2463caadf685a189ed4879100
LALITNEW <100189691+LALITNEW@users.noreply.github.com>
2023-12-21 14:09:34 +0700

    commit

---
+++ line_vendor_automated_SKU/template.py
@@ -0,0 +1,35 @@
+json_object = """
+
+{
+    "to": "{line_user_id}",
+    "messages": [
+        {
+            "type": "flex",
+            "altText": "ติดตามไรเดอร์ง่ายแค่นี้เองหรอ",
+            "contents":
+{
+  "type": "bubble",
+  "size": "giga",
+  "hero": {
+    "type": "image",
+    "url": "https://drive.google.com/uc?export=view&id=17LZ-7tgggU6HWP8zvZXPQ32iwK6XlmGr",
+    "size": "full",
+    "aspectRatio": "4:4",
+    "aspectMode": "cover",
+    "action": {
+      "type": "uri",
+      "uri": "https://bit.ly/3QMp9ir"
+    }
+  }
+}
+
+    }
+  ],
+    "customAggregationUnits": [
+        "RiderLate"
+    ]
+}'
+}
+
+
+"""
