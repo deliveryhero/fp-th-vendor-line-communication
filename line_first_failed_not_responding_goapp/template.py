3f0a4a72d1a5e2df7e667aae3b7bb9c4f2353a53
Worawan Thanasanchiraphong <worawan.thanasanchiraphong@foodpanda.co.th>
2024-01-05 15:03:05 +0700

    DITH-2151 Create a new pipeline for line_first_failed_not_responding_goapp

---
+++ line_first_failed_not_responding_goapp/template.py
@@ -0,0 +1,27 @@
+json_object = """
+
+{
+  "to": "{line_user_id}",
+  "messages": [
+    {
+      "type": "flex",
+      "altText": "แย่แล้ว! มีออเดอร์ถูกปฏิเสธ!",
+      "contents": {
+        "type": "bubble",
+        "size": "giga",
+        "hero": {
+          "type": "image",
+          "url": "https://drive.google.com/uc?export=view&id=1Znn1GuvUSqz-HuJWN8BP5DyUhYO0sbIk",
+          "size": "full",
+          "aspectRatio": "4:4",
+          "aspectMode": "cover"
+        }
+      }
+    }
+  ],
+  "customAggregationUnits": [
+    "NPAOGoapp"
+  ]
+}
+
+"""
