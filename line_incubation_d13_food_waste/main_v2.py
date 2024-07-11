d1a7b0372c963729022975dfcf1dc10257c1054f
b-worawan <worawan.thanasanchiraphong@foodpanda.co.th>
2024-07-11 13:48:02 +0700

    Revert "DITH-2456 Delete test files"
    This reverts commit cc2a34c836cb14efb0503ea02b8fbf8c7441a73a.

---
+++ line_incubation_d13_food_waste/main_v2.py
@@ -0,0 +1,87 @@
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+sys.path.insert(0, parentdir + '/src')
+from bigquery_data import query_BQ_table, record_line_communication_logs
+from send_line_message import send_request_line_api_generic_reminder
+from get_secrete_token import get_secret_data
+import datetime, pytz
+import requests
+from template_v2 import json_object
+
+# Basic configuration tables
+query_table = "fulfillment-dwh-production.pandata_report.country_TH_vendor_ops_incubation_line_verification"
+logs_table_id = "foodpanda-th-bigquery.pandata_th_external.line_communication_logs_live"
+pipeline_name = "line_incubation_d13_food_waste"
+vendor_age = 13
+
+# Basic configuration parameters
+slack_webhook = os.getenv('slack_webhook')
+Live = False
+url = "https://api.line.me/v2/bot/message/push"
+
+token = get_secret_data()
+headers = {'Authorization': "Bearer {" + token + "}", 'Content-Type': 'application/json'}
+tz = pytz.timezone('Asia/Bangkok')
+now = datetime.datetime.now(tz).strftime("%Y-%m-%d %H:%M:%S")
+
+if Live == False:
+    query = f"""
+      SELECT
+        "test" AS vendor_code,
+        "Uca11d4d4585c435204950dba18dafcd8" AS line_user_id,
+      UNION ALL
+      SELECT
+        "test" AS vendor_code,
+        "U0276f51ba4f3f5f0e275ebcdbbc11b6d" AS line_user_id,
+      UNION ALL
+      SELECT
+        "test" AS vendor_code,
+        "U4b2ec72133f4eb87bf9db0ebaa441431" AS line_user_id,
+    """
+
+if Live == True:
+    query = f"""
+      SELECT
+        vendor_code,
+        line_user_id,
+        vendor_age
+      FROM `{query_table}`
+      WHERE line_user_id IS NOT NULL
+        AND vendor_age = {vendor_age}
+        AND vertical = "Restaurant"
+        AND DATE(recorded_at_local) = CURRENT_DATE("Asia/Bangkok")
+    """
+
+try:
+  dataframe = query_BQ_table(query)
+  # print("Created dataframe successfully")
+except BaseException as e:
+  requests.post(slack_webhook,
+  json = {'text' : '*{pipeline_name}*: Failed to get data: ' + str(e)})
+  # print("Cannot create dataframe")
+  # print(e)
+
+try:
+  reponse_code_list, json_list = send_request_line_api_generic_reminder(url = url,
+                                                          headers = headers,
+                                                          json_object = json_object,
+                                                          dataframe = dataframe)
+except BaseException as e:
+  requests.post(slack_webhook,
+  json = {'text' : '*{pipeline_name}*: Failed send API request: ' + str(e)})
+
+try:
+  df = dataframe.filter(items=['vendor_code', 'line_user_id'])
+  df["return_response"] = reponse_code_list
+  df["msg_sent_date_time"] = now
+  df["template_id_if_any"] = pipeline_name
+  df["msg_url"] = url
+  df["msg_content"] = pipeline_name
+  df_records = df.to_dict('records')
+  status = record_line_communication_logs(logs_table_id, df_records)
+except BaseException as e:
+  requests.post(slack_webhook,
+  json = {'text' : '*{pipeline_name}*: Failed to record logs: ' + str(e)})
+  # print(e)
