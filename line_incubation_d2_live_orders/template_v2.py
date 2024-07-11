aa344de28c61de420148fcc4e06e2bfcc3dd3d98
b-worawan <worawan.thanasanchiraphong@foodpanda.co.th>
2024-07-11 09:34:34 +0700

    DITH-2456 Add files for testing

---
+++ line_incubation_d2_live_orders/template_v2.py
@@ -0,0 +1,57 @@
+json_object = """
+
+{
+  "to": "{line_user_id}",
+  "messages": [
+    {
+      "type": "flex",
+      "altText": "จัดการออเดอร์ได้ง่ายๆ",
+      "contents": {
+        "type": "bubble",
+        "size": "giga",
+        "direction": "ltr",
+        "hero": {
+          "type": "image",
+          "url": "https://drive.google.com/uc?export=view&id=16kCMxCEPWOJ2nX4watdFkyl3g0CZjNOX",
+          "align": "center",
+          "gravity": "center",
+          "size": "full",
+          "aspectRatio": "21:21",
+          "aspectMode": "cover"
+        },
+        "body": {
+          "type": "box",
+          "layout": "vertical",
+          "contents": [
+            {
+              "type": "text",
+              "text": "ดูวิดีโอการจัดการออเดอร์ทุกกรณี คลิก!",
+              "size": "xs",
+              "align": "center",
+              "style": "italic",
+              "action": {
+                "type": "uri",
+                "label": "action",
+                "uri": "https://tinyurl.com/AutoCommsytplgd"
+              }
+            }
+          ],
+          "backgroundColor": "#F7C6CC",
+          "cornerRadius": "lg",
+          "paddingAll": "sm",
+          "action": {
+            "type": "uri",
+            "label": "action",
+            "uri": "https://tinyurl.com/AutoCommsytplgd"
+          },
+          "alignItems": "center"
+        }
+      }
+    }
+  ],
+  "customAggregationUnits": [
+    "Incud2"
+  ]
+}
+
+"""
